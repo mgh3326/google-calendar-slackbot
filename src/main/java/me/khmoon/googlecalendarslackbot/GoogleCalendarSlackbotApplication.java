365311b6e2377ea6beca4eb6c71e2583cf3a1696
robin <mgh3326@naver.com>
2020-04-08 10:19:20 +0900

    Initial commit

---
+++ src/main/java/me/khmoon/googlecalendarslackbot/GoogleCalendarSlackbotApplication.java
@@ -0,0 +1,13 @@
+package me.khmoon.googlecalendarslackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarSlackbotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleCalendarSlackbotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/khmoon/googlecalendarslackbot/GoogleCalendarSlackbotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khmoon.googlecalendarslackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarSlackbotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
