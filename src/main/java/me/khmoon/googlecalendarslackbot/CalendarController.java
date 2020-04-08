d03c678a37dc05ef8db645dbbf5778cbb10f6edc
robin <mgh3326@naver.com>
2020-04-08 23:44:45 +0900

    feat: 원하는 날짜에 해당하는 일정 조회하는 기능 추가

--- src/main/java/me/khmoon/googlecalendarslackbot/CalendarController.java
+++ src/main/java/me/khmoon/googlecalendarslackbot/CalendarController.java
@@ -1,5 +1,6 @@
 package me.khmoon.googlecalendarslackbot;
 
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
@@ -10,8 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,15 +27,31 @@ public class CalendarController {
   private final ApplicationProperties applicationProperties;
   private final Calendar calendar;
 
-
   @GetMapping("/find")
-  public ResponseEntity<Events> findAllEvents() throws IOException {
-    Events events = calendar.events().list(applicationProperties.getGoogle().getCalenderId()).execute();
-    for (Event event : events.getItems()) {
+  public ResponseEntity<Events> findAllEvents(@RequestParam String fetchingDate) throws IOException {
+    String calenderId = applicationProperties.getGoogle().getCalenderId();
+    log.debug("fetching date : {}", fetchingDate);
+
+    Calendar.Events eventsInCalendar = calendar.events();
+
+    LocalDate localDate = LocalDate.parse(fetchingDate);
+    LocalDateTime startOfDay = localDate.atStartOfDay();
+    LocalDateTime endOfDay = LocalTime.MAX.atDate(localDate);
+
+    Date startDate = Date.from(startOfDay.atZone(ZoneId.systemDefault()).toInstant());
+    Date endDate = Date.from(endOfDay.atZone(ZoneId.systemDefault()).toInstant());
+
+    Calendar.Events.List eventsList = eventsInCalendar.list(calenderId)
+            .setTimeMin(new DateTime(startDate))
+            .setTimeMax(new DateTime(endDate));
+    Events fetchedSchedules = eventsList.execute();
+
+    for (Event schedule : fetchedSchedules.getItems()) {
       log.info("만든이 - {}, 제목 - {}, 설명 - {}, 위치 - {}, 시작:{}, 끝:{}",
-              event.getCreator(), event.getSummary(), event.getDescription(), event.getLocation(), event.getStart(), event.getEnd());
+              schedule.getCreator(), schedule.getSummary(), schedule.getDescription(), schedule.getLocation()
+              , schedule.getStart(), schedule.getEnd());
     }
 
-    return new ResponseEntity<>(events, HttpStatus.OK);
+    return new ResponseEntity<>(fetchedSchedules, HttpStatus.OK);
   }
 }
