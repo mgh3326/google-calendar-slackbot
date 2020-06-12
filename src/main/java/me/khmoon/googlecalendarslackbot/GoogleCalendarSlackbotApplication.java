d15cbaa7c0def58b006b7451c3f26aaa718a5c4b
robin <mgh3326@naver.com>
2020-06-12 15:27:28 +0900

    object mapper 추가

--- src/main/java/me/khmoon/googlecalendarslackbot/GoogleCalendarSlackbotApplication.java
+++ src/main/java/me/khmoon/googlecalendarslackbot/GoogleCalendarSlackbotApplication.java
@@ -1,7 +1,12 @@
 package me.khmoon.googlecalendarslackbot;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class GoogleCalendarSlackbotApplication {
@@ -10,4 +15,11 @@ public class GoogleCalendarSlackbotApplication {
     SpringApplication.run(GoogleCalendarSlackbotApplication.class, args);
   }
 
+  @Bean
+  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+    ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+    objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    return objectMapper;
+  }
 }
