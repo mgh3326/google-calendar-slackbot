04d11988d7f2fcb6ef4524d363e4e02d03a7e64e
robin <mgh3326@naver.com>
2020-06-22 09:38:03 +0900

    google calendar timeout 변경

--- src/main/java/me/khmoon/googlecalendarslackbot/calendar/WebConfig.java
+++ src/main/java/me/khmoon/googlecalendarslackbot/calendar/WebConfig.java
@@ -1,6 +1,8 @@
 package me.khmoon.googlecalendarslackbot.calendar;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -29,10 +31,26 @@ public class WebConfig {
   @Value("${credential.filepath}")
   private String credentialsFilePath;
 
+  private static HttpRequestInitializer setHttpTimeout(
+          final HttpRequestInitializer requestInitializer) {
+    return new HttpRequestInitializer() {
+      @Override
+      public void initialize(HttpRequest httpRequest) throws IOException {
+        requestInitializer.initialize(httpRequest);
+        // This allows the API to call (and avoid timing out on)
+        // functions that take up to 6 minutes to complete (the maximum
+        // allowed script run time), plus a little overhead.
+        httpRequest.setReadTimeout(380000);
+      }
+    };
+  }
+
   @Bean
   public Calendar calendar() throws GeneralSecurityException, IOException {
     final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-    return new Calendar.Builder(httpTransport, JSON_FACTORY, new HttpCredentialsAdapter(getCredentials()))
+    GoogleCredentials credentials = getCredentials();
+    HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+    return new Calendar.Builder(httpTransport, JSON_FACTORY, setHttpTimeout(requestInitializer))
             .setApplicationName(APPLICATION_NAME)
             .build();
   }
@@ -40,4 +58,5 @@ public class WebConfig {
   private GoogleCredentials getCredentials() throws IOException {
     return GoogleCredentials.fromStream(new ClassPathResource("credentials.json").getInputStream()).createScoped(SCOPES);
   }
+
 }
